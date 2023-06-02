860e6acac416d4dd135db4c8f51600c7adcbbafc
virgel1995 <kazouya44@gmail.com>
2023-06-02 22:43:39 +0300

    up

--- src/components/apps/VSCode.tsx
+++ src/components/apps/VSCode.tsx
@@ -2,7 +2,7 @@ export default function VSCode() {
   return (
     <iframe
       className="h-full w-full bg-vscode"
-      src="https://github1s.com/virgel1995/portfolio/blob/main/README.md"
+      src="https://github.com/virgel1995/website-simulating-macOS-s/blob/main/README.md"
       frameBorder="0"
       title="VSCode"
     />
